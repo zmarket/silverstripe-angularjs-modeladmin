9b8821841877ab31a2379a05ba4223b6ebe9a781
David Craig <david@silverstripe.com>
2014-09-05 14:08:54 +1200

    Added unit test for Product

---
+++ tests/server/product/ProductTest.php
@@ -0,0 +1,9 @@
+<?php
+class ProductTest extends SapphireTest {
+    protected static $fixture_file = 'ProductTest.yml';
+
+    public function testGenerateProductJSON() {
+        $product = $this->objFromFixture('Product', 'apple');
+        $this->assertEquals(')]}\','."\xA".'{"title":"Apple","description":"Apples are yum.","image":{"path":"apple.png","title":"Apple"}}', $product->generateProductJSON());
+    }
+}
